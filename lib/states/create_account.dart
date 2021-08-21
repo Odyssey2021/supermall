b8946b65d3f5800494b2b97ca620ca8d9cde08aa
Winyou <oldsahai@gmail.com>
2021-08-21 08:54:19 +0700

    Update

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -12,7 +12,7 @@ class _CreateAccountState extends State<CreateAccount> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create New Account Winyou'),
+        title: Text('Create New Account'),
       ),
     );
   }
